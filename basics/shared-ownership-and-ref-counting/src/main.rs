3ab75f0da246e01a6f01c2915c58a51d5e7e607c
ChiefLittleLeaf <haddockmason@gmail.com>
2024-03-09 10:46:55 -0600

    Add: Shared ownership and reference counting examples

--- basics/shared-ownership-and-ref-counting/src/main.rs
+++ basics/shared-ownership-and-ref-counting/src/main.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+use std::sync::Arc;
 use std::thread;
 
 fn main() {
@@ -7,7 +9,31 @@ fn main() {
     //thread::spawn(|| dbg!(&X));
 
     //Leaking
-    let x: &'static [i32; 3] = Box::leak(Box::new([1, 2, 3]));
-    thread::spawn(move || dbg!(x));
-    thread::spawn(move || dbg!(x));
+    //let x: &'static [i32; 3] = Box::leak(Box::new([1, 2, 3]));
+    //thread::spawn(move || dbg!(x));
+    //thread::spawn(move || dbg!(x));
+
+    //Reference counting
+    //let a = Rc::new([1, 2, 3]);
+    //let b = a.clone();
+
+    //This is not thread safe
+    //thread::spawn(move || dbg!(b));
+    //assert_eq!(a.as_ptr(), b.as_ptr()); // Same allocation
+
+    //let a = Arc::new([1, 2, 3]);
+    //let b = a.clone();
+
+    //thread::spawn(move || dbg!(a));
+    //thread::spawn(move || dbg!(b));
+
+    //Shadowing recommended for reuse of var naming
+    let a = Arc::new([1, 2, 3]);
+
+    thread::spawn({
+        let a = a.clone();
+        move || dbg!(a)
+    });
+
+    dbg!(a);
 }
